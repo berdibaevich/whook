20694a24a939e58ae9a7e9eb9243aa295fb841bc
berdibaevich <berdibaevich.me@gmail.com>
2026-03-26 23:30:01 +0500

    Imple: using argparse

--- argparse_cli.py
+++ argparse_cli.py
@@ -2,24 +2,18 @@
 
 # https://realpython.com/command-line-interfaces-python-argparse/
 
-import sys
+import argparse
 from pathlib import Path
 
+parser = argparse.ArgumentParser()
 
-if (args_count := len(sys.argv)) > 2:
-    print(f"One argument expected, got {args_count - 1}")
-    raise SystemExit(2)
+parser.add_argument("path")
 
-elif args_count < 2:
-    print("You must specify the target directory")
-    raise SystemExit(2)
+args = parser.parse_args()
 
+target_dir = Path(args.path)
 
-target_dir = Path(sys.argv[1])
-
-print(target_dir)
-
-if not target_dir.is_dir():
+if not target_dir.exists():
     print("The target directory doesn't exist")
     raise SystemExit(1)
 
